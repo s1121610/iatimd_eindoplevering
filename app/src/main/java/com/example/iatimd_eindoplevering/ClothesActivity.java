16f421e6c81c367eacf4cf706e605367e564a942
Max Osewoudt <s1121610@student.hsleiden.nl>
2021-06-30 15:28:08 +0200

    Verbinding met API tot stand gezet.

--- app/src/main/java/com/example/iatimd_eindoplevering/ClothesActivity.java
+++ app/src/main/java/com/example/iatimd_eindoplevering/ClothesActivity.java
@@ -6,7 +6,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Adapter;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ClothesActivity extends AppCompatActivity {
 
@@ -25,12 +37,41 @@ public class ClothesActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.hasFixedSize(); //Betere perf. bij stabiele list length
 
-        Kledingstuk[] kledingstukken = new Kledingstuk[5];
+        final Kledingstuk[] kledingstukken = new Kledingstuk[6]; //mag niet meer veranderd worden
         kledingstukken[0] = new Kledingstuk( "Blauwe korte broek", "korte broek", "zomer", "vakantie","none", 1);
         kledingstukken[1] = new Kledingstuk( "wit t-shirt",  "t-shirt",  "zomer", "vrije tijd", "none", 2);
         kledingstukken[2] = new Kledingstuk( "Donkergroene hoodie", "hoodie", "winter","vrije tijd","none",3);
         kledingstukken[3] = new Kledingstuk( "Zwarte spijkerbroek", "lange broek", "all", "werk", "none", 4);
         kledingstukken[4] = new Kledingstuk( "Superdry groen t-shirt", "t-shirt", "zomer", "vrije tijd", "none", 5);
+        //kledingstukken[5] = new Kledingstuk( "Witte blousse", "blousse", "all", "speciale gelegenheden", "none", 6);
+
+
+        // RequestQueue queue = Volley.newRequestQueue(this);
+        RequestQueue queue = VolleySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
+        String key = "e8edd22274c59149f4da3cea5b2deff7";
+        final String URL = "https://api.openweathermap.org/data/2.5/weather?q=The+hague&appid=" + key;
+
+        //Haal data op uit de API:
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Werkt!", response.toString());
+                        try {
+                            kledingstukken[5] = new Kledingstuk(response.get("name").toString(), response.get("spiecies").toString(), response.get("season").toString(), response.get("occasion").toString(), response.get("image").toString(), 6);
+                            recyclerViewAdapter.notifyDataSetChanged();//De adapter laten weten dat de data is veranderd
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error!", error.getMessage());
+            }
+        });
+
+        VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
 
         recyclerViewAdapter = new kledingstukAdapter(kledingstukken);
         recyclerView.setAdapter(recyclerViewAdapter);
