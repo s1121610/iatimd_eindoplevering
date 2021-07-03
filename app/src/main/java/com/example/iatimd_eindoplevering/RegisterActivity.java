d0f644ed4125c052d39eaee63e8207ed06d6d9e8
Max Osewoudt <s1121610@student.hsleiden.nl>
2021-07-03 12:09:43 +0200

    Registreren werkt

--- app/src/main/java/com/example/iatimd_eindoplevering/RegisterActivity.java
+++ app/src/main/java/com/example/iatimd_eindoplevering/RegisterActivity.java
@@ -11,6 +11,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegisterActivity extends AppCompatActivity {
     EditText name;
     EditText email;
@@ -29,11 +38,47 @@ public class RegisterActivity extends AppCompatActivity {
         repeatPassword = findViewById(R.id.register_HerhaalPassword);
         register = findViewById(R.id.register_button);
 
+        RequestQueue queue = VolleySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
+        final String URL = "http://protected-cliffs-08967.herokuapp.com/api/registration";
+
+
+
         register.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 if(checkDataEntered() == true){
                     Log.d("form validator", "true");
+                    queue.start();
+
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    Toast.makeText(RegisterActivity.this,response,Toast.LENGTH_LONG).show();
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Toast.makeText(RegisterActivity.this,error.toString(),Toast.LENGTH_LONG).show();
+                                }
+                            }){
+                        @Override
+                        protected Map<String,String> getParams(){
+                            Map<String, String> params = new HashMap<>();
+                            params.put("name", name.getText().toString());
+                            params.put("email", email.getText().toString());
+                            params.put("password", password.getText().toString());
+                            params.put("password_confirmation", repeatPassword.getText().toString());
+                            params.put("device_name", "SamsungA71_max");
+                            Log.d("params", String.valueOf(params));
+                            return params;
+                        }
+
+                    };
+                    queue.add(stringRequest);
+                    //VolleySingleton.getInstance().addToRequestQueue(stringRequest);
+
                 }else{
                     Log.d("form validator", "false");
                 }
@@ -52,14 +97,10 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     boolean isEven(EditText psw, EditText rpsw){
-        CharSequence password = psw.getText().toString();
-        CharSequence r_password = rpsw.getText().toString();
-
-
+        String password = psw.getText().toString();
+        String r_password = rpsw.getText().toString();
         if (password == r_password){
-
             return true;
-
         }else {
             Log.d("hiero", (String) password);
             Log.d("hiero", (String) r_password);
@@ -81,10 +122,10 @@ public class RegisterActivity extends AppCompatActivity {
             email.setError("Voer een geldig email adres in.");
             valid = false;
         }
-        else if (isEven(password, repeatPassword) == false){
-            repeatPassword.setError("Dit wachtwoord komt niet overeen met het gekozen wachtwoord.");
-            valid = false;
-        }
+//        else if (isEven(password, repeatPassword) == false){
+//            repeatPassword.setError("Dit wachtwoord komt niet overeen met het gekozen wachtwoord.");
+//            valid = false;
+//        }
         else if (password.length() < 8){
             Toast t = Toast.makeText(this, "Het wachtwoord moet uit ten minste 8 karakters bestaan.", Toast.LENGTH_SHORT);
             t.show();
