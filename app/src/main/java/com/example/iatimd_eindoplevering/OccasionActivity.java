2d826b6b65ae14571b938a7823e5d7365de4c02a
Max Osewoudt <s1121610@student.hsleiden.nl>
2021-07-04 13:11:52 +0200

    Activity aangemaakt

---
+++ app/src/main/java/com/example/iatimd_eindoplevering/OccasionActivity.java
@@ -0,0 +1,14 @@
+package com.example.iatimd_eindoplevering;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OccasionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_occasion);
+    }
+}
